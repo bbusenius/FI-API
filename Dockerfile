f572d147bb309af1ae63268948f0f7eb31529689
Brad Busenius <bradb@neomailbox.ch>
2019-07-20 19:57:49 -0500

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM ubuntu:18.10
+LABEL maintainer="Brad Busenius https://github.com/bbusenius"
+RUN apt-get update
+RUN apt-get install -y python3 python3-dev python3-pip nginx git
+RUN pip3 install uwsgi
+COPY ./ ./FI-API
+WORKDIR ./FI-API
+RUN pip3 install -r requirements.txt
+COPY ./nginx.conf /etc/nginx/sites-enabled/default
+CMD service nginx start && uwsgi -s /tmp/uwsgi.sock --chmod-socket=666 --manage-script-name --mount /=app:app
---
+++ app.py
@@ -0,0 +1,106 @@
+from inspect import signature
+
+import fi
+from diablo_utils import cast_by_type, functs_from_mod
+from flask import Flask, escape, jsonify, render_template, request
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+# All functions from the FI library
+FUN_DICT = functs_from_mod(fi)
+
+
+def error_msg(code: int, msg: str, required: list) -> str:
+    """
+    Generate an error message to display.
+
+    Args:
+        code: Numeric code
+
+        msg:  Message
+
+        required: list of allowed or required items
+
+    Returns:
+        Error message formatted as JSON.
+    """
+    retval = {'error': {'code': code, 'message': {msg: required}}}
+    return jsonify(retval)
+
+
+def get_mod_func_args(fun_params):
+    """
+    Retrieves function arguments from a GET request and casts them to their
+    expected data types for a given module.
+
+    Args:
+        fun_params: parameters from an inspect.signature.
+
+    Returns:
+        A tuple where the first item is a boolean and the second item is a
+        list of mixed data types to pass to a function. The boolean will be
+        set to True if an expected argument is missing. This is considered
+        a fail.
+    """
+    fun_args = []
+    fail = False
+    for key in fun_params.keys():
+        arg_name = fun_params[key].name
+        arg_passed = request.args.get(arg_name)
+        if arg_passed is None:
+            fail = True
+            break
+        expected_type = str(fun_params[key].annotation)
+        arg_to_pass = cast_by_type(request.args.get(arg_name), expected_type)
+        fun_args.append(arg_to_pass)
+    return (fail, fun_args)
+
+
+@app.route('/')
+def home():
+    """
+    Homepage route.
+    """
+    return render_template('home.html')
+
+
+@app.route('/json/')
+def json_home():
+    """
+    JSON API home route.
+    """
+    api = {}
+    for func in FUN_DICT:
+        fun_params = signature(FUN_DICT[func]).parameters
+        api[func] = {}
+        for key in fun_params:
+            api[func][key] = str(fun_params[key].annotation)
+    return jsonify({'Available API Endpoints': api})
+
+
+@app.route('/json/<fun_name>')
+def api_json_endpoints(fun_name):
+    """
+    Endpoints for the JSON API.
+    """
+    fun_name = escape(fun_name)
+    # Invalid endpoint
+    if fun_name not in FUN_DICT:
+        msg = 'Not a valid endpoint. Only the following endpoints are allowed'
+        return error_msg(400, msg, list(FUN_DICT.keys()))
+    # All good
+    else:
+        # Function from the FI library
+        function_to_call = FUN_DICT[fun_name]
+        fun_params = signature(function_to_call).parameters
+        fail, fun_args = get_mod_func_args(fun_params)
+        # Fail - missing GET params
+        if fail:
+            msg = 'You did not pass all of the required GET parameters. The following are required'
+            return error_msg(400, msg, list(fun_params.keys()))
+        # Success! Call the library function and jsonify the result
+        else:
+            return jsonify(function_to_call(*fun_args))

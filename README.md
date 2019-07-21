02c8e24cf70296f321d452034e21221d7ff9753a
Brad Busenius <bradb@neomailbox.ch>
2019-07-21 16:51:40 -0500

    Dead simple unit tests and ci.

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Create a virtual env and install the requirements with:
 
 ```
 pip install -r requirements.txt
+pip install -r requirements_dev.txt
 ```
 
 ### Run the Flask app
@@ -22,6 +23,12 @@ pip install -r requirements.txt
 env FLASK_APP=app.py flask run
 ```
 Visit the site at http://127.0.0.1:5000
+
+### Run unit tests
+
+```
+pytest
+```
 
 ## Run with Docker
 ### Build
---
+++ tests/test_app.py
@@ -0,0 +1,36 @@
+import unittest
+
+from app import app
+
+
+class Tests(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def testHomepageRoute(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testJsonHomeRoute(self):
+        response = self.client.get('/json', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def testBadRoute(self):
+        response = self.client.get('/nada', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
+
+    def testJSONEndpoint(self):
+        with app.app_context():
+            response1 = self.client.get(
+                '/json/take_home_pay?gross_pay=5000&employer_match=1000&taxes_and_fees=3000'
+            )
+            self.assertEqual(response1.get_json(), 3000)
+
+            response2 = self.client.get(
+                '/json/take_home_pay?gross_pay=5000&employer_match=600&taxes_and_fees=2000,500,400'
+            )
+            self.assertEqual(response2.get_json(), 2700)
+
+
+if __name__ == '__main__':
+    unittest.main()
